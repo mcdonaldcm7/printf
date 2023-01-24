2deb3b30470b9fe21e1cb76a5e14b995c7bc22d5
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-24 08:51:49 -0800

    Added convert_number.c, main.h, number.c, _printf.c, and print_functions.c

--- _printf.c
+++ _printf.c
@@ -1,84 +1,49 @@
-#include <unistd.h>
-#include <stdarg.h>
+#include "main.h"
 
 /**
- * lq_strlen - Retrieves the length of a string
+ * _printf - prints anything
+ * @format: the format string
  *
- * @s: Input string
- *
- * Return: Length of s
- */
-int lq_strlen(char *s)
-{
-	if (s == NULL || s[0] == 0)
-		return (0);
-	return (1 + lq_strlen(&s[1]));
-}
-
-/**
- * _prints - Prints a string to the standard output
- *
- * @s: Input string
- *
- * Return: Number of bytes written to STDOUT
- */
-int _prints(char *s)
-{
-	if (s == NULL)
-		return (0);
-	return (write(STDOUT_FILENO, s, lq_strlen(s)));
-}
-
-/**
- * _printc - Prints a string to the standard output
- *
- * @c: Input character
- *
- * Return: Number of bytes written to STDOUT (1)
- */
-int _printc(char c)
-{
-	return (write(STDOUT_FILENO, &c, 1));
-}
-
-/**
- * _printf - Prints the arguments passed to the standard output
- *
- * @format: Character string to be outputted to the standard output
- *
- * Return: Number of bytes written to the stdout
+ * Return: number of bytes printed
  */
 int _printf(const char *format, ...)
 {
-	int count;
-	va_list args;
+	int sum = 0;
+	va_list ap;
+	char *p, *start;
+	params_t params = PARAMS_INIT;
 
-	count = 0;
-	va_start(args, format);
-	while (format != NULL && *format)
+	va_start(ap, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)
 	{
-		char tmp;
-
-		tmp = *format++;
-		if (tmp == '%')
+		init_params(&params, ap);
+		if (*p != '%')
 		{
-			tmp = *format++;
-			switch (tmp)
-			{
-				case 'c':
-					count += _printc(va_arg(args, int));
-					break;
-				case 's':
-					count += _prints(va_arg(args, char *));
-					break;
-				default:
-					break;
-			}
-		} else
-		{
-			count += _printc(tmp);
+			sum += _putchar(*p);
+			continue;
 		}
+		start = p;
+		p++;
+		while (get_flag(p, &params)) /* while char at p is flag char */
+		{
+			p++; /* next char */
+		}
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			sum += print_from_to(start, p,
+				params.l_modifier || params.h_modifier ? p - 1 : 0);
+		else
+			sum += get_print_func(p, ap, &params);
 	}
-	va_end(args);
-	return (count);
+	_putchar(BUF_FLUSH);
+	va_end(ap);
+	return (sum);
 }
